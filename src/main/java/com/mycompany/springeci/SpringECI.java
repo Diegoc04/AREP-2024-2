881a8537a774c702f95a6e087e7f0fa465ce69e0
Diegoc04 <diego.fercho.castellanos@gmail.com>
2024-09-01 16:10:03 -0500

    Urls

--- src/main/java/com/mycompany/springeci/SpringECI.java
+++ src/main/java/com/mycompany/springeci/SpringECI.java
@@ -1,13 +1,19 @@
+package com.mycompany.springeci;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.springeci;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +23,7 @@ import java.util.Map;
  */
 public class SpringECI {
 
-    public static void main(String[] args) throws ClassNotFoundException, MalformedURLException, IllegalAccessException, InvocationTargetException {
+    public static void main(String[] args) throws ClassNotFoundException, MalformedURLException, IllegalAccessException, InvocationTargetException, IOException {
         Class c = Class.forName(args[0]);
         Map<String, Method> services = new HashMap();
         
@@ -32,59 +38,42 @@ public class SpringECI {
             }
         }
         
-        // Codigo quemado para ejemplo
-        
-        URL serviceurl = new URL("http://localhost:8080/App/hello");
-        String path = serviceurl.getPath();
-        System.out.println("Path " + path);
-        String servicename = path.substring(4);
-        System.out.println("Service name " + servicename);
-        
-        Method ms = services.get(servicename);
-        ms.invoke(ms);
+        // Escuchar solicitudes HTTP
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Servidor escuchando en puerto 8080...");
 
-        // Codigo quemado para ejemplo
-        
-        serviceurl = new URL("http://localhost:8080/App/pi");
-        path = serviceurl.getPath();
-        System.out.println("Path " + path);
-        servicename = path.substring(4);
-        System.out.println("Service name " + servicename);
-        
-        ms = services.get(servicename);
-        ms.invoke(ms);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-        // Codigo quemado para ejemplo
-        
-        serviceurl = new URL("http://localhost:8080/App/luz");
-        path = serviceurl.getPath();
-        System.out.println("Path " + path);
-        servicename = path.substring(4);
-        System.out.println("Service name " + servicename);
-        
-        ms = services.get(servicename);
-        ms.invoke(ms);
+            // Leer solicitud HTTP
+            String requestLine = in.readLine();
+            if (requestLine != null) {
+                System.out.println("Solicitud recibida: " + requestLine);
+                String[] requestParts = requestLine.split(" ");
+                String path = requestParts[1]; // Obtén el path de la solicitud
+                String servicename = path.substring(4); // Asumiendo que el prefijo es "/App/"
+                System.out.println("Service name: " + servicename);
 
-        // Codigo quemado para ejemplo
-        
-        serviceurl = new URL("http://localhost:8080/App/bye");
-        path = serviceurl.getPath();
-        System.out.println("Path " + path);
-        servicename = path.substring(4);
-        System.out.println("Service name " + servicename);
-        
-        ms = services.get(servicename);
-        ms.invoke(ms);
-
-        // Codigo quemado para ejemplo
-        
-        serviceurl = new URL("http://localhost:8080/App/planck");
-        path = serviceurl.getPath();
-        System.out.println("Path " + path);
-        servicename = path.substring(4);
-        System.out.println("Service name " + servicename);
-        
-        ms = services.get(servicename);
-        ms.invoke(ms);
+                Method ms = services.get(servicename);
+                if (ms != null) {
+                    String response = (String) ms.invoke(null); // Invocar método
+                    // Enviar respuesta HTTP
+                    out.write("HTTP/1.1 200 OK\r\n");
+                    out.write("Content-Type: text/plain\r\n");
+                    out.write("\r\n");
+                    out.write(response);
+                } else {
+                    // Responder con 404 si el servicio no existe
+                    out.write("HTTP/1.1 404 Not Found\r\n");
+                    out.write("Content-Type: text/plain\r\n");
+                    out.write("\r\n");
+                    out.write("Servicio no encontrado");
+                }
+                out.flush();
+            }
+            clientSocket.close();
+        }
     }
 }
